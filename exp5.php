febbd8bbf90833e1edc1dae9edfdb2f1df106d12
Narendran C <40605746+narendran25@users.noreply.github.com>
2018-07-24 11:18:28 +0530

    Add files via upload

---
+++ exp5.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        class Car {
+    function Car() {
+        $this->model = "VW";
+    }
+}
+
+
+$herbie = new Car();
+
+
+echo $herbie->model;
+        ?>
+    </body>
+</html>
